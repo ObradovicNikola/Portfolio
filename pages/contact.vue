94baab070eb0c5e34dec75889ab10e1ceacfc73a
Obradovic Nikola <obradovicnikola009@gmail.com>
2020-11-12 11:38:13 +0100

    lamba function url

--- pages/contact.vue
+++ pages/contact.vue
@@ -103,18 +103,15 @@ function data() {
 const methods = {
   async submitForm() {
     try {
-      const res = await this.$axios.$post('https://obradovicnikola.com/.netlify/functions/mailjet', {
+      const res = await this.$axios.$post('/.netlify/functions/mailjet', {
         name: this.name,
         recipient: this.email,
         message: this.message,
       })
-      this.response = res
-      this.error = null
       console.log('form sent')
       // this.formSent = true
     } catch (e) {
-      this.error = e.response
-      this.response = '—'
+      console.log(e)
     }
   },
 }
